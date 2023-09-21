caee1cf4ba50ff8e5a22fdd3ed74430d47276a7d
Nicole Liang <nliang@stu.naperville203.org>
2023-09-21 14:20:21 -0500

    Sieve of Eratosthenes started

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -11,9 +11,12 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
+        Set<int> numbers = new HashSet<>();
 
-        // Your work goes here
-        . . .
+        for (int i = 2; i <= n; i++)
+        {
+            
+        }
 
 
 
--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,27 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes/src/war-and-peace.txt");
+
+        // Print all the words present in the novel but not in the dictionary
+        for (String word: novelWords)
+        {
+            if (!dictionaryWords.contains(word))
+                System.out.println(word);
+        }
+
+        // Print the number of unique words in the novel
+        System.out.println("There are " + novelWords.size() + " unique words in the novel.");
+
+        // Print the number of unique words with > 3 letters
+        Iterator<String> iter = novelWords.iterator();
+        while (iter.hasNext())
+        {
+            if (iter.next().length() > 3)
+                iter.remove();  // ConcurrentModification Error only occurs if not removing with iterator
+        }
+        System.out.println("There are " + novelWords.size() + " unique words with more than three letters in the novel.");
     }
 
     /**
@@ -25,6 +46,25 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        // Using HashSet instead of TreeSet since order doesn't matter
+        Set<String> words = new HashSet<>();
+
+        // Determine the current working directory
+        System.out.println(System.getProperty("user.dir"));
+
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        // Use any character that is not a letter as delimeters
+        in.useDelimiter("[^a-zA-z]+");
+
+        while (in.hasNext())
+        {
+            // Add words to the set (duplicates ignored)
+            // lowercase prevents two of the same words (capitalized differently) from being added to the list
+            words.add(in.next().toLowerCase()); 
+
+        }
+
+        return words;
     }
 }
