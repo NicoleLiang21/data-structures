907e93583094786bb5fb186ba62d6b7b22c7484e
Nicole Liang <nliang@stu.naperville203.org>
2023-09-28 14:20:46 -0500

    Gradebook finished

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-. . .
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,29 +15,53 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> students = new HashMap<>();
+
 
         boolean done = false;
         while(!done)
         {
             System.out.println("A)dd R)emove M)odify P)rint Q)uit");
             String input = in.next().toUpperCase();
+            String name, grade;
+
             if (input.equals("Q"))
             {
                 done = true;
-            } else if (input.equals("A"))
+            } else if (input.equals("A") || input.equals("M"))
             {
-                . . .
+                // Get the student's name
+                System.out.println("Name of student: ");
+                name = in.next();
+                in.nextLine();
+
+                // Get the student's grade
+                System.out.println("Grade: ");
+                grade = in.next();
+                in.nextLine();
+
+                // Add the student's information to the HashMap
+                students.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
-            } else if (input.equals("M"))
-            {
-                . . .
+                // Get the name of the student
+                System.out.println("Name of student: ");
+                name = in.next();
+                in.nextLine();
+
+                // remove the student from the HashMap
+                students.remove(name);
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                // Iterate through the list of students (as a set) and print out their names and grades
+                Set<String> keys = students.keySet();
+                for (String key : keys)
+                {s
+                    System.out.println(key + " (" + key.hashCode() + "):" + students.get(key));
+                }
+
             } else
             {
                 done = true;
