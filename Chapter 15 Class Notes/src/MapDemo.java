585cd2ed742585b5dbb809d10d47b24269f86524
Nicole Liang <nliang@stu.naperville203.org>
2023-09-28 14:06:15 -0500

    Map notes

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,29 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /* Map interface is generic
+         * First type = key
+         * Second type = value
+         */
+        Map<String, Color> favColors = new HashMap<>();
+        
+        // Add elements to a map using the put method
+        favColors.put("Peter", Color.black);
+        favColors.put("Seth", Color.blue);
+        favColors.put("Celeste", Color.orange);
+        
+        // 2 different keys may have the same value
+        favColors.put("Dr. Miller", Color.orange);
+
+        // Same key CANNOT have two different values
+        // Using put on an existing key changes its value
+        favColors.put("Peter", Color.yellow);
+
+        // Create a set of the keys in a map
+        Set<String> keys = favColors.keySet();
+        for (String key : keys)
+        {
+            System.out.println(key + " (" + key.hashCode() + "):" + favColors.get(key));
+        }
     }
 }
