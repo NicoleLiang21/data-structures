4d7580d6425de7d27945c95a978dd611bf984b52
Nicole Liang <nliang@stu.naperville203.org>
2023-10-03 14:13:02 -0500

    Stacks, Queues, Priority Queues

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,21 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a to-do list
+        // The WordOrder class has a priority and a description
+        Queue<WorkOrder> todo = new PriorityQueue<>();
+
+        todo.add(new WorkOrder(3, "Water Plants"));
+        todo.add(new WorkOrder(2, "Make Dinner"));
+        todo.add(new WorkOrder(2, "Conquer World"));
+        todo.add(new WorkOrder(9, "Play Video games"));
+        todo.add(new WorkOrder(1, "Study for the Ch. 15 Test"));
+
+        // Objects are not stored in priority order
+        System.out.println(todo);
+
+        // Objects will be removed in priority order
+        while (todo.size() > 0)
+            System.out.println(todo.remove());
     }
 }
