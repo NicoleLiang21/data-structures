0733c21cca459d90db97f28ae594fa0131f3d3ef
Nicole Liang <nliang@stu.naperville203.org>
2023-10-05 20:31:11 -0500

    FloodFill completed

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,40 +1,46 @@
-import java.lang.reflect.Array;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    private Scanner in = new Scanner(System.in);
-    private int row, col, count = 0;
     private Pair pair;
     private Stack<Pair> unfilled = new Stack<>();
-    private int[][] array;
 
     /**
      * Flood fill, starting with the given row and column.
     */
-    public void floodfill(int row, int column)
+    public void floodfill(int row, int col)
     {
-        System.out.print("What is the row number? ");
-        row = in.nextInt();
-        in.next();
-
-        System.out.print("What is the column number? ");
-        col = in.nextInt();
-
         pair = new Pair(row, col);
         unfilled.push(pair);
 
+        int count = 1;
         while (unfilled.size() > 0)
         {
             pair = unfilled.pop();
 
-            Pair north = 
-            
-            if (array[pair.getRow() - 1][pair.getColumn()] == 0) // North
-                unfilled.push
+            if (pixels[pair.getRow()][pair.getColumn()] == 0)
+            {
+                pixels[pair.getRow()][pair.getColumn()] = count;
+                count++;
+            }
+
+            // North
+            if (pair.getRow() != 0 && pixels[pair.getRow() - 1][pair.getColumn()] == 0)
+              unfilled.push(new Pair(pair.getRow() - 1, pair.getColumn()));
+
+            // East
+            if (pair.getColumn() != 9 && pixels[pair.getRow()][pair.getColumn() + 1] == 0)
+              unfilled.push(new Pair(pair.getRow(), pair.getColumn() + 1));
+
+            // South
+            if (pair.getRow() != 9 && pixels[pair.getRow() + 1][pair.getColumn()] == 0)
+              unfilled.push(new Pair(pair.getRow() + 1, pair.getColumn()));
+
+            // West
+            if (pair.getColumn() != 0 && pixels[pair.getRow()][pair.getColumn() - 1] == 0)
+              unfilled.push(new Pair(pair.getRow(), pair.getColumn() - 1));
         }
     }
 
--- Chapter 15 Activities/FloodFill/src/Pair.java
+++ Chapter 15 Activities/FloodFill/src/Pair.java
@@ -2,11 +2,13 @@ public class Pair
 {
     private final int row;
     private final int col;
+    private int order;
 
     public Pair(int row, int col)
     {
         this.row = row;
         this.col = col;
+        this.order = -1;
     }
 
     public int getRow()
