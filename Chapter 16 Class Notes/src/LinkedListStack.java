43b13bf0b4d43a036a39fe4a166c50b14244c1c8
Nicole Liang <nliang@stu.naperville203.org>
2023-10-20 13:50:54 -0500

    LinkedListStack implemented

--- Chapter 16 Class Notes/src/LinkedListStack.java
+++ Chapter 16 Class Notes/src/LinkedListStack.java
@@ -1,3 +1,4 @@
+import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
 
 /**
@@ -7,6 +8,7 @@ public class LinkedListStack
 {
     private Node first;
 
+
     /**
      * Constructs an empty stack.
     */
@@ -15,27 +17,42 @@ public class LinkedListStack
         first = null;
     }
 
+
     /**
      * Adds an element to the top of the stack.
      *
      * @param element the element to add
     */
+    public void push(Object element)
+    {
+        Node newNode = new Node();
+        newNode.data = element;
 
+        // Put the new node on top of the stack -- the next element was the first element
+        newNode.next = this.first;
 
+        // Update the status of the new element on top of the stack
+        this.first = newNode;
 
-
+    }
 
 
     /**
         Removes the element from the top of the stack.
         @return the removed element
-    */
+     */
+    public Object pop()
+    {
+        if (this.empty())
+            return new NoSuchElementException(); 
 
+        Object element = this.first.data;
 
+        // Replace the top (removed) element with the net element
+        this.first = this.first.next;
 
-
-
-
+        return element;
+    }
 
 
     /**
@@ -43,6 +60,10 @@ public class LinkedListStack
      *
      * @return true if the stack is empty
     */
+    public boolean empty()
+    {
+        return this.first.data == null;
+    }
 
 
     static class Node
