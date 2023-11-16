5ba8a7f38e0e2f6bc05fc1be2b776a08451b3d13
Nicole Liang <111467644+NicoleLiang21@users.noreply.github.com>
2023-11-16 14:19:35 -0600

    preorder method

--- Chapter 17 Class Notes/src/Tree.java
+++ Chapter 17 Class Notes/src/Tree.java
@@ -62,4 +62,38 @@ public class Tree
 
 
     // Additional methods will be added in later sections.
+
+    /*
+     * A visitor whose visit method is called for each visited node during a tree traversal
+     */
+    public interface Visitor
+    {
+        /**
+         * visit method called for each node
+         * @param data - the data for neach node being visited
+         */
+        void visit (Object data);
+
+        /**
+         * Traverses this tree in preorder
+         * @param v - visitor to be invoked on each node
+         */
+        public void preorder(Visitor v);
+
+        /**
+         * Traverses the tree with a given root in preorder
+         * @param n - root of the tree to traverse
+         * @param v - visitor invoked on each node
+         */
+        private static void preorder(Node n, Visitor v)
+        {
+            if (n == null)
+                return;
+            
+            v.visit(n.data);
+
+            for (Node child : n.children)
+                preorder(child, v);
+        }
+    }
 }
